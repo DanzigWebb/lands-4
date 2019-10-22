9588c336df669e9db202a86173a317ef63236427
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2019-10-22 12:27:00 +0300

    c

--- js/common.js
+++ js/common.js
@@ -1,7 +1,17 @@
-var swiper = new Swiper('.swiper-container', {
+var swiper = new Swiper('.sect5__slider', {
   pagination: {
-    el: '.swiper-pagination',
-    type: 'fraction',
+    el: '.sect5__pag',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoHeight: true,
+  spaceBetween: 40
+});
+var swiper2 = new Swiper('.sect7__slider', {
+  pagination: {
+    el: '.sect7__pag',
   },
   navigation: {
     nextEl: '.swiper-button-next',
